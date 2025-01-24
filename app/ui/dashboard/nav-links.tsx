e8cbf7581992991ed96ea0931ae4b011bf618b71
boris-dev24 <borisngoko92@gmail.com>
2025-01-24 18:23:32 -0500

    commit chapitre6

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -4,16 +4,24 @@ import {
   UserGroupIcon,
   HomeIcon,
   DocumentDuplicateIcon,
+  CogIcon,
 } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import clsx from 'clsx';
  
-// Liste des liens (si elle est définie ailleurs, assure-toi qu'elle est importée)
+// Map of links to display in the side navigation.
+// Depending on the size of the application, this would be stored in a database.
 const links = [
   { name: 'Home', href: '/dashboard', icon: HomeIcon },
-  { name: 'Invoices', href: '/dashboard/invoices', icon: DocumentDuplicateIcon },
+  {
+    name: 'Invoices',
+    href: '/dashboard/invoices',
+    icon: DocumentDuplicateIcon,
+  },
   { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
+  { name: 'Admin', href: '/dashboard/admin', icon: CogIcon },
+ 
 ];
  
 export default function NavLinks() {
